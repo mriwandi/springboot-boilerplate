ade2a1f028de0902e8b4ad284f2d225e4a0dd4cb
muhammad.riwandi <muhammad.riwandi@gdn-commerce.com>
2025-05-20 09:15:02 +0700

    Initial Spring Boot setup

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController {
+	private final UserRepository userRepository;
+
+	public UserController(UserRepository userRepository) {this.userRepository = userRepository;}
+
+	@GetMapping
+	public List<User> getAllUsers() {
+		return userRepository.findAll();
+	}
+
+	@PostMapping
+	public User createUser(@RequestBody User user) {
+		return userRepository.save(user);
+	}
+}
